7864ecae0abd4760a420da1d3791fc5ee78eff45
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-05-17 20:39:30 -0400

    Vista Usuarios añadida

--- tarea5/src/App.vue
+++ tarea5/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Usuarios/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Usuarios from './components/Usuarios.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Usuarios
   }
 }
 </script>
