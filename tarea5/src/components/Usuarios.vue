19fdf1d3c51114b71db04c1424048fb126f1650c
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-05-17 20:53:41 -0400

    Hard code añadido a la página principal

--- tarea5/src/components/Usuarios.vue
+++ tarea5/src/components/Usuarios.vue
@@ -1,6 +1,91 @@
 <template>
     <div>
       <h1>Lista de Usuarios</h1>
+
+      <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre(s)</th>
+                <th scope="col">Género</th>
+                <th scope="col">País</th>
+                <th scope="col">Imagen</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Hombre</td>
+                <td>Estados Unidos</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Hombre</td>
+                <td>Inglaterra</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Jeny</td>
+                <td>Mujer</td>
+                <td>Australia</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Pepe</td>
+                <td>Hombre</td>
+                <td>España</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Juan</td>
+                <td>Hombre</td>
+                <td>México</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>Catalina</td>
+                <td>Mujer</td>
+                <td>Argentina</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>Valentina</td>
+                <td>Mujer</td>
+                <td>Chile</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td>José</td>
+                <td>Hombre</td>
+                <td>Uruguay</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">9</th>
+                <td>Joao</td>
+                <td>Hombre</td>
+                <td>Brasil</td>
+                <td>Imagen</td>
+            </tr>
+            <tr>
+                <th scope="row">10</th>
+                <td>Angélica</td>
+                <td>Mujer</td>
+                <td>Colombia</td>
+                <td>Imagen</td>
+            </tr>
+        </tbody>
+</table>
+
     </div>
   </template>
   
