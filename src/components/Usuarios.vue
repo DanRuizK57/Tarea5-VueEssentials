ffe0f1ec5520b3cf067f84eba9be3fe3a385b33e
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-05-18 15:19:13 -0400

    Hard code de variables añadido

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -10,15 +10,19 @@
                 <th scope="col">Género</th>
                 <th scope="col">País</th>
                 <th scope="col">Imagen</th>
+                <th scope="col">Rango de Edad</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <th scope="row">1</th>
                 <td>{{nombre}}</td>
-                <td>Hombre</td>
-                <td>Estados Unidos</td>
-                <td>Imagen</td>
+                <td>{{genero}}</td>
+                <td>{{pais}}</td>
+                <td>
+                    <img :src="imagen" alt="Imagen de usuario" />    
+                </td>
+                <td>{{rangoEdad}}</td>
             </tr>
             <tr>
                 <th scope="row">2</th>
@@ -26,6 +30,7 @@
                 <td>Hombre</td>
                 <td>Inglaterra</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">3</th>
@@ -33,6 +38,7 @@
                 <td>Mujer</td>
                 <td>Australia</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">4</th>
@@ -40,6 +46,7 @@
                 <td>Hombre</td>
                 <td>España</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">5</th>
@@ -47,6 +54,7 @@
                 <td>Hombre</td>
                 <td>México</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">6</th>
@@ -54,6 +62,7 @@
                 <td>Mujer</td>
                 <td>Argentina</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">7</th>
@@ -61,6 +70,7 @@
                 <td>Mujer</td>
                 <td>Chile</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">8</th>
@@ -68,6 +78,7 @@
                 <td>Hombre</td>
                 <td>Uruguay</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">9</th>
@@ -75,6 +86,7 @@
                 <td>Hombre</td>
                 <td>Brasil</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
             <tr>
                 <th scope="row">10</th>
@@ -82,6 +94,7 @@
                 <td>Mujer</td>
                 <td>Colombia</td>
                 <td>Imagen</td>
+                <td>Mayor de Edad (+18)</td>
             </tr>
         </tbody>
 </table>
@@ -93,16 +106,35 @@
     import usuarios from '../json/users.json'
 
     const miss = usuarios.filter(usuario => usuario.name.title === "Miss")
-    const nombre = miss[3].name.title
+    let nombre = miss[3].name.title + " " + miss[3].name.first + " " + miss[3].name.last
+    let genero = miss[3].gender
+    let pais = miss[3].location.country
+    let imagen = miss[3].picture.medium
+    let rangoEdad;
+
+    if (miss[3].dob.age > 18){
+        rangoEdad = "Mayor de edad (+18)"
+    } else {
+        rangoEdad = "Menor de edad (-18)"
+    }
+
   export default {
     name: 'ListaUsuarios',
     data() {
         return {
-            nombre: ""
+            nombre: "",
+            genero: "",
+            pais: "",
+            imagen: "",
+            rangoEdad: ""
         };
     },
     mounted() {
         this.nombre = nombre;
+        this.genero = genero;
+        this.pais = pais;
+        this.imagen = imagen;
+        this.rangoEdad = rangoEdad;
     }
   }
   </script>
